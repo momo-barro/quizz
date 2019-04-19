4f10ab55b175469abd53d500fccd20a003b8990f
momo <sadiobarro@gmail.com>
2019-04-19 18:38:11 +0000

    questions fetched from the api

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template slot="lead">
+            {{ currentQuestion.question }}
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            List of answers
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button @click="next" variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    },
+    mounted: {
+        answers() {
+            let answers = [...currentQuestion.incorrect_answers]
+        }
+    }
+}
+</script>
