fa2d46443b76b3e548c6be378f508620bbda00fc
momo <sadiobarro@gmail.com>
2019-04-23 16:54:40 +0000

    have to correct the counter bug isCorrect causing problems

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,26 +7,108 @@
 
             <hr class="my-4">
 
-            <p>
-            List of answers
+            <p  >                
+                <b-list-group>
+                <b-list-group-item
+                v-for="(answer, index) in answers"
+                :key="index"
+                @click="selectAnswer(index)"
+                :class="[selectedIndex === index ? 'selected' : '']"
+                >
+                    {{ answer }}
+                </b-list-group-item>
+                </b-list-group>
             </p>
 
-            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button            
+                variant="primary"
+                @click="submitAnswer"
+                :disabled="selectedIndex === null || answered"
+            >
+                Submit
+            </b-button>
             <b-button @click="next" variant="success" href="#">Next</b-button>
         </b-jumbotron>
     </div>
 </template>
 
 <script>
+import _ from "lodash"
+// import { truncate } from 'fs';
+
 export default {
     props: {
         currentQuestion: Object,
-        next: Function
+        next: Function,
+        increment: Function
     },
-    mounted: {
+    data() {
+        return {
+            selectedIndex: null,
+            correctIndex :null,
+            shuffledAnswers: [],
+            answered: false
+        }
+    },
+    computed: {
         answers() {
-            let answers = [...currentQuestion.incorrect_answers]
+            let answers = [...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers
+        }
+    },
+    watch: {
+        currentQuestion: {
+            immediate: true,
+            handler(){
+                this.selectedIndex = null
+                //this.answered = false
+                this.shuffleAnswers()
+            }
+        }
+    },
+    methods: {
+        selectAnswer(index) {
+            this.selectedIndex = index
+        },
+        submitAnswer() {
+            let isCorrect = false
+
+            if(this.selectedIndex === this.correctIndex) {
+                isCorrect = true
+            }
+
+            this.answered = true
+
+            this.increments(isCorrect)
+        },
+        shuffleAnswers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]            
+            this.shuffledAnswers = _.shuffle(answers)
         }
     }
 }
 </script>
+
+<style scoped>
+.list-group {
+    margin-bottom: 15px;
+}
+.list-group-item:hover {
+    background: #EEE;
+    cursor: pointer;
+}
+.btn{
+    margin: 0 5px;
+}
+.selected{
+    background-color: lightblue;
+}
+.correct{
+    background-color: lightgreen;
+}
+.incorrect{
+    background-color: red;
+}
+
+</style>
